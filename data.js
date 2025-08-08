852de8a0dc95cbb1f3af405899355890514e67fd
Richange <richi.e@outlook.com>
2025-08-08 20:05:42 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+const albums = [
+    { front: "bilder/1-front.jpg", back: "bilder/1-back.jpg", discs: 2 },
+    { front: "bilder/2-front.jpg", back: "bilder/2-back.jpg", discs: 1 },
+    { front: "bilder/3-front.jpg", back: "bilder/3-back.jpg", discs: 2 },
+    { front: "bilder/4-front.jpg", back: "bilder/4-back.jpg", discs: 2 },
+    { front: "bilder/5-front.jpg", back: "bilder/5-back.jpg", discs: 2 },
+    { front: "bilder/6-front.jpg", back: "bilder/6-back.jpg", discs: 2 },
+]
